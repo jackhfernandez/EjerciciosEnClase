7d0e4fdb01380879679c3a359bfb6730fec5d887
unknown <132464970+jackhfernandez@users.noreply.github.com>
2025-05-14 14:12:40 -0500

    Agregando proyecto 05_estructuras_combinadas

---
+++ 05_estructuras_combinadas/src/main/java/com/mycompany/_estructuras_combinadas/App.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany._estructuras_combinadas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jack
+ */
+
+// 1. Crear clase datosFamiliares
+// llenar (dato familiar) y mostrar
+class datosFamiliares {
+  // Atributos
+  String direccion;
+  int numHijos;
+  String distrito;
+}
+
+// 2. Crear clase empleados (5 atributos)
+// llenar empleado, mostrar
+class empleado {
+  // Atributos
+  String dni;
+  String nombres;
+  int edad;
+  String areaTrabajo;
+}
+
+// 3. en ele main, crear el vector empresa
+// cons MAX -> 3
+
+public class App {
+
+  // Scanner y Const
+  static Scanner scanner = new Scanner(System.in);
+  static final int MAX = 3;
+  
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+
+}
