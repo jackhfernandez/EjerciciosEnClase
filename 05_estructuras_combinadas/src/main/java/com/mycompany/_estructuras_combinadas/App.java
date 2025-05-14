f31a5a95bc9cc7b5bbef54416693468757f31670
unknown <132464970+jackhfernandez@users.noreply.github.com>
2025-05-14 15:54:23 -0500

    Terminando

--- 05_estructuras_combinadas/src/main/java/com/mycompany/_estructuras_combinadas/App.java
+++ 05_estructuras_combinadas/src/main/java/com/mycompany/_estructuras_combinadas/App.java
@@ -1,5 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+/**
+ * Crear un programa con las siguientes condiciones:
+ * crear dos clases: DatosFamiliares y Empleado
+ * DatosFamiliares:
+ * - Direccion
+ * - NumeroHijos
+ * - Distrito
+ * 
  */
 package com.mycompany._estructuras_combinadas;
 
@@ -12,7 +18,7 @@ import java.util.Scanner;
 
 // 1. Crear clase datosFamiliares
 // llenar (dato familiar) y mostrar
-class datosFamiliares {
+class DatosFamiliares {
   // Atributos
   String direccion;
   int numHijos;
@@ -21,25 +27,84 @@ class datosFamiliares {
 
 // 2. Crear clase empleados (5 atributos)
 // llenar empleado, mostrar
-class empleado {
+class Empleado {
   // Atributos
   String dni;
   String nombres;
   int edad;
   String areaTrabajo;
+  DatosFamiliares df;
 }
-
-// 3. en ele main, crear el vector empresa
-// cons MAX -> 3
 
 public class App {
 
   // Scanner y Const
   static Scanner scanner = new Scanner(System.in);
-  static final int MAX = 3;
+  static final int MAX = 2;
   
+  // 3. en ele main, crear el vector empresa
+  // cons MAX -> 3
   public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+    // Vector empresa
+    Empleado empresa[] = new Empleado[MAX];
 
+    // Llenar empleados
+    llenarEmpresa(empresa);
+    
+    // Mostrar 
+    mostrarEmpresa(empresa);
+  }
+  
+  // Llenar datos familiares
+  static void llenarDatosFamiliares(DatosFamiliares df){
+    System.out.println("\nIngresar datos familiares");
+    System.out.println("Direccion: ");
+    df.direccion = scanner.next();
+    System.out.println("Nro Hijos: ");
+    df.numHijos = scanner.nextInt();
+    System.out.println("Distrito: ");
+    df.distrito = scanner.next();
+  }
+  
+  // Mostrar datos familiares
+  static void mostrarDatosFamiliares(DatosFamiliares df){
+    System.out.println("\nMostrando datos familiares");
+    System.out.println("Direccion: " + df.direccion + "Distrito: " + df.distrito + "Nro hijos: " + df.numHijos);
+  }
+  
+  // Llenar datos Familiares
+  static void llenarEmpleado(Empleado empleado){
+    System.out.println("LLenar datos del empleado");
+    System.out.print("DNI: ");
+    empleado.dni = scanner.next();
+    System.out.print("NOMBRE: ");
+    empleado.nombres = scanner.next();
+    System.out.print("EDAD: ");
+    empleado.edad = scanner.nextInt();
+    System.out.print("Area Trabajo: ");
+    empleado.areaTrabajo = scanner.next();
+    empleado.df = new DatosFamiliares();
+    llenarDatosFamiliares(empleado.df);
+  }
+  
+  // Mostrar empleado
+  static void mostrarEmpleado(Empleado empleado){
+    System.out.println("NOMBRE: "+empleado.nombres+" DNI: "+empleado.dni+" EDAD: "+empleado.edad+ " Area Trabajo: "+empleado.areaTrabajo);
+  }
+  
+  // LLenar empresa
+  static void llenarEmpresa(Empleado empresa[]){
+    for (int i = 0; i < MAX; i++) {
+      System.out.println("Empleado " + (i+1));
+      empresa[i] = new Empleado();
+      llenarEmpleado(empresa[i]);
+    }
+  }
+  
+  // Mostrar empresa
+  static void mostrarEmpresa(Empleado empresa[]){
+    for (int i = 0; i < MAX; i++) {
+      mostrarEmpleado(empresa[i]);
+    }
+  }
 }
